34d5d17487b27851a33cd9d12666ba253bc82430
Érico Andrei <ericof@gmail.com>
2023-11-09 15:22:03 -0300

    Fix package generation

--- plone_addon/{{ cookiecutter.python_package_name }}/src/{{ cookiecutter.__package_namespace }}/{{ cookiecutter.__package_name }}/__init__.py
+++ plone_addon/{{ cookiecutter.python_package_name }}/src/{{ cookiecutter.__package_namespace }}/{{ cookiecutter.__package_name }}/__init__.py
@@ -1,8 +1,10 @@
 """Init and utils."""
 import logging
-
 from zope.i18nmessageid import MessageFactory
 
-_ = MessageFactory("{{ cookiecutter.python_package_name }}")
 
-logger = logging.getLogger("{{ cookiecutter.python_package_name }}")
+PACKAGE_NAME = "{{ cookiecutter.python_package_name }}"
+
+_ = MessageFactory(PACKAGE_NAME)
+
+logger = logging.getLogger(PACKAGE_NAME)
